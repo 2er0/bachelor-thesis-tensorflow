ea029da3c1988650c3b10aa8397e8e06c2a40d0e
2er0 <2er0_gh@dbaumi.at>
2017-07-23 09:48:26 +0200

    final version of BA1 & BA2 part 2

--- run.sh
+++ run.sh
@@ -4,3 +4,9 @@ biber _DaBa.bcf
 #bibtex _DaBa.tex
 pdflatex _DaBa.tex
 pdflatex _DaBa.tex
+
+pdflatex _DaBa_Teil2.tex
+biber _DaBa_Teil2.bcf
+#bibtex _DaBa_Teil2.tex
+pdflatex _DaBa_Teil2.tex
+pdflatex _DaBa_Teil2.tex
